534741ff07964d5d3eeb268db18617d63a99ca3a
c0dered <okaschuk@gmail.com>
2021-09-18 16:17:35 +0300

    Initial commit

---
+++ src/main/java/ru/mosmatic/mosmngr/MosmngrApplication.java
@@ -0,0 +1,13 @@
+package ru.mosmatic.mosmngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosmngrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosmngrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mosmatic/mosmngr/MosmngrApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mosmatic.mosmngr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosmngrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
